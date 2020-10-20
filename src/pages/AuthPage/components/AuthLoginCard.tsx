6015f0648dcbd31e4892a5ad3f9caff4031951dc
ansskf <anss.khaled00@gmail.com>
2020-10-20 17:29:00 +0200

    Design Login/Signup cards

--- src/pages/AuthPage/components/AuthLoginCard.tsx
+++ src/pages/AuthPage/components/AuthLoginCard.tsx
@@ -1,10 +1,27 @@
 import React, { FunctionComponent } from 'react'
+import { Card, Button } from 'react-bootstrap'
+
+import { FormInput } from '../../../core/components/FormInput/FormInput'
+import './AuthPage.scss'
 
 type TAuthLoginCardComponentProps = {
 }
 
 export const AuthLoginCardComponent: FunctionComponent<TAuthLoginCardComponentProps> = () => {
     return (
-            <div>LoginCardWorks</div>
+        <Card className="c__card shadow p-3 rounded-0 border-0">
+
+            <div className="">
+                <div className="text-center font-weight-lighter p-3 c__card__title mb-3">
+                    LOGIN
+                </div>
+                <div>
+                    <FormInput title="Email" type="text" required />
+                    <FormInput title="Password" type="password" required />
+                    <Button variant="outline-primary" block>Submit</Button>
+                </div>
+            </div>
+
+        </Card>
     )
 }
--- src/pages/AuthPage/components/AuthRegisterCard.tsx
+++ src/pages/AuthPage/components/AuthRegisterCard.tsx
@@ -1,10 +1,29 @@
 import React, { FunctionComponent } from 'react'
+import { Card, Button } from 'react-bootstrap'
+
+import { FormInput } from '../../../core/components/FormInput/FormInput'
+import './AuthPage.scss'
 
 type TAuthRegisterCardComponentProps = {
 }
 
 export const AuthRegisterCardComponent: FunctionComponent<TAuthRegisterCardComponentProps> = () => {
     return (
-            <div>RegisterCardWorks</div>
+        <Card className="c__card shadow p-3 rounded-0 border-0">
+
+            <div className="">
+                <div className="text-center font-weight-lighter p-3 c__card__title mb-3">
+                    Register
+                </div>
+                <div>
+                    <FormInput title="Name" type="text" required />
+                    <FormInput title="Email" type="text" required />
+                    <FormInput title="Password" type="password" required />
+                    <FormInput title="Confirm Password" type="password" required />
+                    <Button variant="outline-primary" block>Submit</Button>
+                </div>
+            </div>
+
+        </Card>
     )
 }
